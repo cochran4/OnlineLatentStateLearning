6f4a5b7c5b42f9309f2996fb55e96c8ad47bd4af
cochran4 <47640953+cochran4@users.noreply.github.com>
2019-05-04 14:55:26 -0500

    Add files via upload

--- Common Files/EstimateLogLike.m
+++ Common Files/EstimateLogLike.m
@@ -1,47 +1,44 @@
-function [logL,MU,var,L0,L0All] = EstimateLogLike(par,dta,approach) %#ok<INUSL>
+function [logL,MU,agent,logL0,lsb] = EstimateLogLike(par,dta,approach) %#ok<INUSL>
 % Estimate log-likelihood
 %   Input:
 %           par      = structure of parameters
 %           dta      = structure containing experimental data
 %           approach = name of learning model 
 %   Output:
-%           logL     = likelihood of participant behavior by trial
-%           MU       = Expected mean outcome by arm and reward dimensions
-%           var      = structure of all variables
-%           logL     = log-likelihood of participant behavior
+%           logL     = log-likelihood of participant behavior given
+%           parameters
 
 % Initialize variables 
-var      = eval([approach,'(''Initialize'',par)']);
+agent  = eval([approach,'(par,[])']);
 logL   = 0;
 
 % Loop through trials
 for t = 1:par.ntrials
 
     % Get rewards and feature vector
-    par.t         = t;           % Add in current time
-    var(t).c_vec  = reshape( dta.c_vec(t,:),par.D,par.C,par.L*par.ncop,par.A );
-        
-    % Get expectations per arm
-    mu       = eval([approach,'(''Estimate'',par,var)']);
-            
-    % Save mu
-    MU(t,:) = mu(:)';
+    par.t        = t;           % Add in current time
+    world.c_vec  = reshape( dta.c_vec(t,:),par.D,par.C,par.ls.ncop,par.A );
+    world.win    = reshape( repmat(dta.R(t,:),1,par.A), par.D, par.A );        
+    world.arm    = dta.arm(t);
     
-    % Soft-max probability
-    p   = exp( par.tau*mu );
-    p   = p/sum(p);
+    % Update agent
+    agent        = eval([approach,'(par,agent,world)']);
     
-    % Get relevant information
-    var(t).win = dta.R(t,:);
-    var(t).arm = dta.arm(t);
+    % Get probability of arm choice
+    [~,p]        = SoftMaxChoice(agent.mu,par);
     
-    % Log-probability / probability of arm choices
-    logL       = logL + log( p( dta.arm(t) ) );
-    L0(t,1)    = p( dta.arm(t) );
-    L0All(t,:) = p;
     
-    % Update variables
-    var(t+1) =  eval([approach,'(''Update'',par,var)']); 
+    % Log-probability of picked arm
+    logL       = logL + log( p( world.arm ) );
+    logL0(t,1) = p( world.arm );
+    
+    % Save expectations
+    MU(t,:) = agent.mu(:)';
+    
+    % Save beliefs
+    if isfield(agent,'lsb')
+        lsb(t,:) = agent.lsb(:)';
+    end
 
 end
 
